70626abd2973a147de5d53d22d7a9303bb3364e0
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-12-18 09:23:13 +0200

    U3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import yfinance as yf
+import datetime as dt
 
 app = Flask(__name__)
 
@@ -10,4 +11,7 @@ def hello_world():
 
 @app.route('/about')
 def about():
-    return yf.download("AAPL", "2023-01-01", "2023-01-02")
+    startdate = dt.datetime.strptime("2023-01-01", "%Y-%m-%d").date()
+    enddate = dt.datetime.strptime("2023-01-02", "%Y-%m-%d").date()
+
+    return yf.download("AAPL", start=startdate, end=enddate, interval="1d")
