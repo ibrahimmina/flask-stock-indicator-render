7a2b8fbc8f0e18d0bc279b6c0f53b16aae0d69d8
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-12-18 09:12:35 +0200

    U1

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import yfinance as yf
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/about')
+def about():
+    return yf.download("AAPL", "2023-01-01", "2023-01-02")
